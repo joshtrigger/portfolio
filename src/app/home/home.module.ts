f437734072fdd62ce7fd2bb5c4ff46e77064d3b5
joshtrigger <lugjosh@gmail.com>
2021-04-29 16:30:00 +0300

    about me

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,15 +6,25 @@ import { HomeComponent } from './home/home.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { AvatarModule } from 'ngx-avatar';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    AboutMeComponent,
+    ResumeComponent,
+    ContactsComponent,
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule,
     FlexLayoutModule,
-    MatCardModule,
     AvatarModule,
+    MatCardModule,
+    MatButtonModule,
   ],
 })
 export class HomeModule {}
