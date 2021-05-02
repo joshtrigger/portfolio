fefa6a578236e9621f124845a72276048827f23a
joshtrigger <lugjosh@gmail.com>
2021-05-03 01:14:06 +0300

    [WIP] resume module

--- src/app/home/resume/resume.component.ts
+++ src/app/home/resume/resume.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import * as data from '../../data.json';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.scss']
+  styleUrls: ['./resume.component.scss'],
 })
 export class ResumeComponent implements OnInit {
+  resume: any = (data as any).default;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
